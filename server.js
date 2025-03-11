12bf324c715d2c54845374dc858f869161b3a968
jarkhhh <121623503+jarkhhh@users.noreply.github.com>
2025-03-11 16:20:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const WebSocket = require('ws');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/chat', { useNewUrlParser: true, useUnifiedTopology: true });
+const messageSchema = new mongoose.Schema({ 
+    user: String, 
+    message: String, 
+    
+});
+const Message = mongoose.model('Message', messageSchema);
+
 const server = new WebSocket.Server({ port: 8080 });
 let messages = [];
+//read from database
+messages =  Message.find()
 server.on('connection', ws => {
     console.log('Новое соединение');
     ws.on('message', msg => {
@@ -11,6 +22,10 @@ server.on('connection', ws => {
         }
         if (parsed.command === 'send') {
             messages.push(parsed.data);
+            let message = new Message(parsed.data);
+            message.save(function (err) {
+                if (err) return console.error(err);
+            });
         }
     });
 });
